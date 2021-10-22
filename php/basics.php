6cd7f699959f2e00b0d9c0c1b211494afe10d259
ellastyko <vadimsergeev1337@gmail.com>
2021-10-22 16:20:29 +0300

    minor fixes

---
+++ php/basics.php
@@ -0,0 +1,16 @@
+<?php
+
+    # How to run php script ...@device: php basics.php 
+
+    /*
+        There are 8 data types in PHP
+    */
+
+    $str = "some text"; # string
+    $int = 5;          # integer
+    $double = 5.5;          # double / float
+    $arr = [1, 3, 5];   # array
+    $null = null;       # NULL
+    echo gettype($float);
+
+    
